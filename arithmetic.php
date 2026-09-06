bf67e194c846ee3f4123c593f3e3e7839862f3b7
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-06 16:40:45 +0545

    arithmetic operators in php

---
+++ arithmetic.php
@@ -0,0 +1,16 @@
+<?php
+$num1 = 15;
+$num2 = 10;
+
+echo ($num1 + $num2) . "<br>"; 
+echo ($num1 - $num2) . "<br>";
+echo ($num1 * $num2) . "<br>";
+echo ($num1 / $num2) . "<br>";
+echo ($num1 % $num2) . "<br>"; // shows the remainder
+echo ($num1 ** $num2) . "<br>"; // returns num1 to the power num2 as 
+                                // it is using an exponential operator
+
+
+
+
+?>
