7d23b1ccdd05ed46666b8d87b616abf13628aff5
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-07 13:27:15 +0545

    assignment operator in php

---
+++ AssignmentOperators.php
@@ -0,0 +1,15 @@
+<?php
+// assignment operators in php
+
+$num1 = 4;
+$num2 = 3;
+
+echo ($num1 += $num2) . "<br>"; 
+echo ($num1 -= $num2) . "<br>";
+echo ($num1 = $num2) . "<br>";
+echo ($num1 *= $num2) . "<br>";
+echo ($num1 /= $num2) . "<br>";
+echo ($num1 %= $num2) ."<br>";
+echo ($num1 **= $num2) . "<br>";
+
+?>
