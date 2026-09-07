fec4306db34150f3e4adfdc2dc5817acc2ed1e6b
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-07 14:52:27 +0545

    tring Operations in php

---
+++ StringOperators.php
@@ -0,0 +1,19 @@
+<?php
+//String Operations in php
+
+// . is used for contacenation
+// .=  is used for contacentation assignment
+
+$num1 = 15;
+$num2 = 67;
+
+echo $num1. " " . $num2 . "<br>"; // gives 15 67 as we used the contacenation operation
+
+$info = "My name is ";
+echo $info . "<br>";
+
+$info.= "Raman.";
+echo $info; // gives My name is Raman.
+
+
+?>
