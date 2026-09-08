f5c6de2ce6d00299e52b61b61712a398daca5ec8
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-08 16:48:15 +0545

    indexed arrays in php

---
+++ Arrays.php
@@ -0,0 +1,29 @@
+<?php
+
+// arrays in php
+// arrays are of 3 types in php 
+// They are: 1) Indexed Arrays
+//           2) Assoiciative Arrays
+//           3) Multi-Dimensional Arrays
+
+
+// code demonstration of indexed arrays in php
+
+// We can create arrays with 2 ways, they are: 
+// $array = array("Xiaomi", "Poco", "Honor", 55000); // first way
+
+$array = ["Xiaomi", "Poco", "Honor", 55000]; // second way, you can use any method as you like
+
+// for accessing elements we can use indexing here, and the indexing is always start from  0 in php
+
+// echo $array[0];
+// echo $array[2];
+
+// we can use the following to print all elements at once
+print_r($array);
+
+
+
+
+
+?>
