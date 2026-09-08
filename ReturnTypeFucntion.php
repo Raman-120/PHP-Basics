13f6f26b4a5e4e63afc72c8b0ade0acfbc327220
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-08 14:59:20 +0545

    return type function in php

---
+++ ReturnTypeFucntion.php
@@ -0,0 +1,13 @@
+<?php
+// functions that return specific value instead of printing it directly
+
+//code demonstration of functions with return keyword
+function multiply($a, $b){
+    return $a * $b;
+}
+
+echo multiply(5,10); 
+
+// the return keyword sends the value where the function is called.
+
+?>
