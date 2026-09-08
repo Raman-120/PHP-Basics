9da66f17c76f8edafebff9c72497a55bdc1c1e8a
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-08 17:04:48 +0545

    assoiciative arrays with for each loop in php

--- Arrays.php
+++ Arrays.php
@@ -2,8 +2,8 @@
 
 // arrays in php
 // arrays are of 3 types in php 
-// They are: 1) Indexed Arrays
-//           2) Assoiciative Arrays
+// They are: 1) Indexed Arrays => uses index number to access elements
+//           2) Assoiciative Arrays => Uses string keys instead of numbers
 //           3) Multi-Dimensional Arrays
 
 
@@ -20,10 +20,30 @@ $array = ["Xiaomi", "Poco", "Honor", 55000]; // second way, you can use any meth
 // echo $array[2];
 
 // we can use the following to print all elements at once
-print_r($array);
+// print_r($array);
 
 
+// Asssoiciative arrays in php
 
+$info = [
+    "name" => "XYG",
+    "age" => 20,
+    "course" => "BIT"
+];
+
+// echo $info["name"]; // here, name , age and course are string keys, and program access the data from the srtring key
+// //                      rather than any index number.
+// print_r($info);
+
+//another way to print the result is using for each loop,
+
+foreach ($info as $information){
+    echo $information . "<br>";
+}
+
+foreach($array as $arr){
+    echo $arr . "<br>";
+}
 
 
 ?>
