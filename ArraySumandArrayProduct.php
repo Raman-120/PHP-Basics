b1cede837010f3a668c71115afd59169768a5861
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-10 14:09:57 +0545

    array sum and array product in php

---
+++ ArraySumandArrayProduct.php
@@ -0,0 +1,13 @@
+<?php
+// array sum and array product in php with built in fucntions
+
+$numbers = [1,2,3,4,5];
+
+echo array_sum($numbers) ."<br>";
+echo array_product($numbers) . "<br>";
+
+$values = ["num1" => 1, "num2" => 2, "num3" => 4];
+
+echo array_sum($values) . "<br>";
+echo array_product($values) . "<br>";
+?>
