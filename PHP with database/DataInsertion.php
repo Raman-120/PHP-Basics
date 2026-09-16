a88a5b29606d2258b70186caf8c6991ce59c9f23
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-16 14:42:32 +0545

    created a basic form for data insertion

---
+++ PHP with database/DataInsertion.php
@@ -0,0 +1,27 @@
+<?php
+
+    // demonstrating how to insert data in the database
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action  = "values.php" method="POST">
+        <label>Username</label>
+        <input type="text" name = "user"> <br>
+        <label>password</label>
+        <input type="password" name = "pass"> <br>
+        <input type = "submit" name="" value = "Insert Data">
+        </form>
+    
+</body>
+</html>
